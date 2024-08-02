b91c07c16e1281f3061f91567b012c7d1e052ee6
PotterArjun <arjun.prajapati@qspear.com>
2024-08-02 16:17:33 +0530

    url type changed .

--- src/main/java/com/delivery/controller/LocationController.java
+++ src/main/java/com/delivery/controller/LocationController.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +18,7 @@ public class LocationController {
 	@Autowired
 	private KafkaService kafkaService;
 	
-	@PostMapping("/update")
+	@GetMapping("/update")
 	public ResponseEntity<?> updateLocation(){
 		
 //		this.kafkaService.updateLocation("( "+Math.round(Math.random()*1000)+""+" ,  "+Math.round(Math.random()*1000)+")");
